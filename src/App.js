aa5f08d06a02769bd09087cdaabd9ea590392564
hoangtranson <son.hoang01@gmail.com>
2022-06-16 21:35:35 +0700

    move test components to react tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import StateTutorial from "./components/useState";
-import ReducerTutorial from "./components/useReducer";
-import EffectTutorial from "./components/useEffect";
-import RefTutorial from "./components/useRef";
-import LayoutEffectTutorial from "./components/useLayoutEffect";
-import ImpreativeHandle from "./components/useImperativeHandle";
-import ContextTutorial from "./components/contextTutorial";
-import MemoTutorial from "./components/useMemo";
-import CallBackTutorial from "./components/useCallback";
 
 function App() {
 
   return (
     <div className="App">
-      {/* <StateTutorial></StateTutorial> */}
-      {/* <ReducerTutorial /> */}
-      {/* <EffectTutorial></EffectTutorial> */}
-      {/* <RefTutorial /> */}
-      {/* <LayoutEffectTutorial /> */}
-      {/* <ImpreativeHandle /> */}
-      {/* <ContextTutorial /> */}
-      {/* <MemoTutorial /> */}
-      <CallBackTutorial />
+      Hoang test
     </div>
   );
 }
